c13c1f3c387aec4abb29ed07eb779a2c90886ac4
Subhimukbel <subhi.mukbel@live.com>
2024-10-04 22:32:40 +0400

    enabling mouse and mic

--- Frontend/implementations/typescript/src/player.ts
+++ Frontend/implementations/typescript/src/player.ts
@@ -1,6 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-import { Config, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.4';
+import { Config, Flags, PixelStreaming } from '@epicgames-ps/lib-pixelstreamingfrontend-ue5.4';
 import { Application, PixelStreamingApplicationStyle } from '@epicgames-ps/lib-pixelstreamingfrontend-ui-ue5.4';
 const PixelStreamingApplicationStyles =
     new PixelStreamingApplicationStyle();
@@ -17,6 +17,8 @@ document.body.onload = function() {
 
 	// Create a config object
 	const config = new Config({ useUrlParams: true });
+	config.setFlagEnabled(Flags.UseMic, true);
+	config.setFlagEnabled(Flags.HoveringMouseMode, true);
 
 	// Create a Native DOM delegate instance that implements the Delegate interface class
 	const stream = new PixelStreaming(config);
